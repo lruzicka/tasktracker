5542b7f6d1cc75de82b836c59e6abd7e9205829d
Lukas Ruzicka <lruzicka@redhat.com>
2021-02-18 00:16:32 +0100

    Fix button colors

--- tasktracker-gui.py
+++ tasktracker-gui.py
@@ -4,41 +4,6 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import * 
 
-# this is a function to get the user input from the text input box
-def getInputBoxValue():
-	userInput = descrip.get()
-	return userInput
-
-
-# this is a function to get the user input from the text input box
-def getInputBoxValue():
-	userInput = ttype.get()
-	return userInput
-
-
-# this is a function to get the user input from the text input box
-def getInputBoxValue():
-	userInput = pgroup.get()
-	return userInput
-
-
-# this is a function to get the user input from the text input box
-def getInputBoxValue():
-	userInput = qasection.get()
-	return userInput
-
-
-# this is a function to get the user input from the text input box
-def getInputBoxValue():
-	userInput = keywords.get()
-	return userInput
-
-
-# this is a function to get the user input from the text input box
-def getInputBoxValue():
-	userInput = link.get()
-	return userInput
-
 def getCheckboxValue():
 	checkedOrNot = markbool.get()
 	return checkedOrNot
@@ -75,32 +40,32 @@ root = Tk()
 
 # This is the section of code which creates the main window
 #root.geometry('867x542')
-root.configure(background='#F0F8FF')
+#root.configure(background='#EEEEEE')
 root.title('Weekly Highlights TaskTracker')
 
 markbool = tk.IntVar()
 
 # Frames - Info, Text, Buttons
-entries = Frame(root, bg='#F0F8FF')
+entries = Frame(root)
 entries.grid(column=0,row=0, padx=5,pady=5)
 
-buttons = Frame(root, bg='#F0F8FF')
+buttons = Frame(root)
 buttons.grid(column=0,row=2, columnspan=2, pady=10)
 
-text = Frame(root, bg='#F0F8FF')
+text = Frame(root)
 text.grid(column=1,row=0, padx=5, pady=5, rowspan=2)
 
-search = Frame(root, bg='#F0F8FF')
+search = Frame(root)
 search.grid(column=0, row=1, padx=5, pady=5)
 
 
 # This is the section of code which creates the a label
-Label(entries, text='Task description:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=0, sticky=(W))
-Label(entries, text='Task type:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=1, sticky=(W))
-Label(entries, text='Task Project group:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=2, sticky=(W))
-Label(entries, text='Fedora QA section:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=3, sticky=(W))
-Label(entries, text='Task Keywords:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=4, sticky=(W))
-Label(entries, text='Link to task:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=5, sticky=(W))
+Label(entries, text='Task description:', font=('arial', 12, 'normal')).grid(column=0,row=0, sticky=(W))
+Label(entries, text='Task type:', font=('arial', 12, 'normal')).grid(column=0,row=1, sticky=(W))
+Label(entries, text='Task Project group:', font=('arial', 12, 'normal')).grid(column=0,row=2, sticky=(W))
+Label(entries, text='Fedora QA section:', font=('arial', 12, 'normal')).grid(column=0,row=3, sticky=(W))
+Label(entries, text='Task Keywords:', font=('arial', 12, 'normal')).grid(column=0,row=4, sticky=(W))
+Label(entries, text='Link to task:', font=('arial', 12, 'normal')).grid(column=0,row=5, sticky=(W))
 
 
 # This is the section of code which creates a text input box
@@ -124,9 +89,9 @@ link.grid(column=1,row=5, sticky=(W))
 
 
 # This is the section of code which creates a button
-Button(buttons, text='Save task', bg='#22CFAA', font=('arial', 12, 'normal'), command=btn_save).grid(column=0,row=0, padx=3)
-Button(buttons, text='Show tasks', bg='#88FFAA', font=('arial', 12, 'normal'), command=btn_show).grid(column=1,row=0, padx=3)
-Button(buttons, text='Copy text', bg='#AACFAA', font=('arial', 12, 'normal'), command=btn_copy).grid(column=2,row=0, padx=3)
+Button(buttons, text='Save task', bg='#FFAAAA', font=('arial', 12, 'normal'), command=btn_save).grid(column=0,row=0, padx=3)
+Button(buttons, text='Show tasks', bg='#AAFFAA', font=('arial', 12, 'normal'), command=btn_show).grid(column=1,row=0, padx=3)
+Button(buttons, text='Copy text', bg='#EEEEEE', font=('arial', 12, 'normal'), command=btn_copy).grid(column=2,row=0, padx=3)
 
 
 # This defines the text field
@@ -134,8 +99,8 @@ textfield = Text(text)
 textfield.grid(column=0, row=0)
 
 # This defines the search criteria
-Label(entries, text='History units:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=6, sticky=(W))
-Label(entries, text='Number of units:', bg='#F0F8FF', font=('arial', 12, 'normal')).grid(column=0,row=7, sticky=(W))
+Label(entries, text='History units:', font=('arial', 12, 'normal')).grid(column=0,row=6, sticky=(W))
+Label(entries, text='Number of units:', font=('arial', 12, 'normal')).grid(column=0,row=7, sticky=(W))
 
 # This is the section of code which creates a listbox
 unittype = Entry(entries, width=50)
@@ -145,7 +110,7 @@ unitnumber = Entry(entries, width=50)
 unitnumber.grid(column=1, row=7, sticky=(W))
 
 # This is the section of code which creates a checkbox
-CheckBoxOne=Checkbutton(entries, text='Markdown', variable=markbool, bg='#F0F8FF', font=('arial', 12, 'normal'))
+CheckBoxOne=Checkbutton(entries, text='Markdown', variable=markbool, font=('arial', 12, 'normal'))
 CheckBoxOne.grid(column=1, row=8, pady=5, sticky=(W))
 
 root.mainloop()
